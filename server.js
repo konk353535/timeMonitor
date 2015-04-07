9285324583c40e5e2d93294e1b1c546ff2b8b9f2
Jason <konk353535@hotmail.com>
2015-04-07 12:34:26 +1000

    Added mongoose require, tested connection to mongodb

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ var app = express()
 
 // Request for API calls
 var request = require('request');
+
+// Require Mongoose DB
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/champions');
+// Connect to Mongoose DB
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function (callback) {
+  console.log("Connected to MongoDB");
+});
  
 app.get('/', function (req, res) {
   res.send('Hello World')
