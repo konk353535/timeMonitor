0f488760a28522c7c0df965164d31a083d190d19
Jason <konk353535@hotmail.com>
2015-04-07 12:31:35 +1000

    Added request require for API calls

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var express = require('express')
 var app = express()
+
+// Request for API calls
+var request = require('request');
  
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
  
-console.log("Listening on port 3000");
+
 app.listen(3000)
+console.log("Listening on port 3000");
