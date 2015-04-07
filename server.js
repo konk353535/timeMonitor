9aeac0c3afe22822b4337e2b748b5b1effdf0532
Jason <konk353535@hotmail.com>
2015-04-07 13:53:44 +1000

    Cleaned up getSummonerId function, added console log for err handling

--- server.js
+++ server.js
@@ -20,30 +20,33 @@ mongoose.connect('mongodb://localhost/champions');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function (callback) {
-  console.log("Connected to MongoDB");
+	console.log("Connected to MongoDB");
 });
  
 app.get('/', function (req, res) {
-  res.send('Hello World')
+	res.send('Hello World')
 })
 
 app.post('/newPlayer', function (req, res) {
-  console.log("Captain transmission recieved - Private Name - " + req.body.name + " - Battalion - "  + req.body.server);
-  getSummonerId(req.body.name, req.body.server);
-  res.send("Roger that private, info recieved");
+	console.log("Captain transmission recieved - Private Name - " + req.body.name + " - Battalion - "  + req.body.server);
+	getSummonerId(req.body.name, req.body.server);
+	res.send("Roger that private, info recieved");
 });
 
 // New function takes Summoner_Name + Server_Name
 // Returns Summoner_ID or err
 
- function getSummonerId(summonerName, serverName){
-	// Request Games From Dev Challenge End Point
+function getSummonerId(summonerName, serverName){
+	// Request Summoner ID
 	request('https://' + serverName + '.api.pvp.net/api/lol/' + serverName + '/v1.4/summoner/by-name/' + summonerName + '?api_key=' + config.apikey , function (error, response, body) {
 		if (!error && response.statusCode == 200) {
-			console.log(body);
+			console.log("Data Recievied");
+		}
+		else {
+			console.log("Invalid Summoner Name or Server");
 		}
 	});
- }
+}
 
 app.listen(3000)
 console.log("Listening on port 3000");
