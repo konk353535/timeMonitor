42162aa04a9abcccc077c2a4b1948e200b430113
Jason <konk353535@hotmail.com>
2015-04-07 22:57:38 +1000

    Added game model, so we can add new games from server.js

---
+++ models/gameModel.js
@@ -0,0 +1,22 @@
+var mongoose = require("mongoose");
+
+// Mongoose User Schema
+// Define schema
+var GameSchema = mongoose.Schema({
+    matchId  :  { type: Number, index: true}
+  , dateTime  :  { type: String}
+  , duration   :  { type: Number }
+  , champion :  { type: Number }
+  , position : {type: Number}
+  // top = pos 1, role 4
+  // mid = pos 2, role 4
+  // JG = pos 3
+  // Supp = pos 4, role 2
+  // Adc = pos 4, role 3
+});
+
+var gameModel = mongoose.model('games', GameSchema);
+
+module.exports = {
+	gameModel: gameModel
+}
