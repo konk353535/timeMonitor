30bbd801ab132ee0011198189be9b90a245acdcf
Jason <konk353535@hotmail.com>
2015-04-07 13:14:29 +1000

    Log values of summoner name input and server dropdown, set default value of server to oce

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,11 +1,13 @@
 var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
 
-
+// Set default select value to oce
+$scope.player = {server: "oce", name: ""};
 
 // Function that is called once add is hit
 $scope.addContact = function(){
-	console.log($scope.contact.name);
+	console.log($scope.player.name);
+	console.log($scope.player.server);
 };
 
 
