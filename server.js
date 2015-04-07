434a82ca57a599e424011cc98d14c81c451ab5a1
Jason <konk353535@hotmail.com>
2015-04-07 13:44:33 +1000

    Added dummy function for getSummonerID

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+// Config file for apiKey ect
+var config = require('./config/config');
+
 var express = require('express')
 var app = express()
 
@@ -28,7 +31,20 @@ app.post('/newPlayer', function (req, res) {
   console.log("Captain transmission recieved - Private Name - " + req.body.name + " - Battalion - "  + req.body.server);
   res.send("Roger that private, info recieved");
 });
- 
+
+// New function takes Summoner_Name + Server_Name
+// Returns Summoner_ID or err
+
+ function getSummonerId(summonerName, serverName){
+	// Request Games From Dev Challenge End Point
+	request('https://oce.api.pvp.net/api/lol/oce/v4.1/game/ids?beginDate=' + epoch_timer + '&api_key=' + config.apikey , function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			console.log(body);
+			// Once game list recieved, pass to game handler
+			Game_Manager(null, body, Game_Extractor);
+		}
+	});
+ }
 
 app.listen(3000)
 console.log("Listening on port 3000");
