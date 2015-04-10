942b11147ed62ffcbf39f3f604d8e2910df14367
Jason <konk353535@hotmail.com>
2015-04-10 20:08:01 +1000

    Added new controller for graph in development weekGraph

--- public/controllers/graphController.js
+++ public/controllers/graphController.js
@@ -1,12 +1,32 @@
 // Access specific angular chart js
-myApp.controller("ChartCtrl", function ($scope, $http) {
+myApp.controller("todayChartCtrl", function ($scope, $http) {
 	// Request server for graph information
 	$scope.getGraph = function(){
 
 		// Gets clients timezone offset
 		var offset = new Date().getTimezoneOffset();
 		// Request server for todays graph data
-		$http.post('/graph', {userOffSet: offset}).success(function(response){
+		$http.post('/graph', {userOffSet: offset, graphType: "today"}).success(function(response){
+			$scope.data = [response];
+		});
+	}
+
+	// Today graph options
+	$scope.labels = ['12', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '1', '2','3','4','5','6','7','8','9','10','11'];
+	$scope.series = ['Last 24 Hours'];
+	$scope.data = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]];
+	$scope.options = {
+	      showTooltips: true
+	    };
+});
+myApp.controller("weekChartCtrl", function ($scope, $http) {
+	// Request server for graph information
+	$scope.getGraph = function(){
+
+		// Gets clients timezone offset
+		var offset = new Date().getTimezoneOffset();
+		// Request server for todays graph data
+		$http.post('/graph', {userOffSet: offset, graphType: "today"}).success(function(response){
 			$scope.data = [response];
 		});
 	}
