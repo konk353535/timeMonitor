5d9a34a818683807633e560cd00dc8fbefc18d00
Jason <konk353535@hotmail.com>
2015-04-11 13:25:54 +1000

    sooo many changes :(. added daysGraph function, to graph multi day periods of time, messed around with timezones still not sure if the timezones are working :(

--- graphManager.js
+++ graphManager.js
@@ -5,25 +5,29 @@ var gameModel = require("./models/gameModel.js").gameModel;
 var responder;
 var offSet;
 
-var moment = require('moment');
+var moment = require('moment-timezone');
 
-var getGraph = function getGraph(userName, serverName, graphName, timeZoneOffSet, res){
+var getGraph = function getGraph(userName, serverName, graphOptions,  res){
 	/*
 	Given user name, server and GRAPH name
 	Will pass information on to appropriate grapher
 	*/
 	responder = res;
-	offSet = timeZoneOffSet;
+	offSet = graphOptions.userOffSet;
 
-	if(graphName == "today"){
-		getUserAndGraph(userName, serverName, todayGraph);
+	if(graphOptions.graphType == "today"){
+		getUserAndGraph(userName, serverName, graphOptions, todayGraph);
+	}
+	else if(graphOptions.graphType == "daysGraph"){
+		getUserAndGraph(userName, serverName, graphOptions, daysGraph);
+		
 	}
 	else {
 		console.log("Error incorrect graph name specified for getGraph(user, server, graphName, res)");
 	}
 }
 
-function getUserAndGraph(userName, serverName, graphFunctionName){
+function getUserAndGraph(userName, serverName, graphOptions, graphFunctionName){
 	/*
 	Gets unique id for user
 	Send's user to specified graph function
@@ -31,7 +35,7 @@ function getUserAndGraph(userName, serverName, graphFunctionName){
 	userModel.findOne({"summonerName":userName, "server":serverName}).exec(function (err, userData) {
 		if (err) return console.error(err);
 		if(userData !== null){
-			graphFunctionName(userData);
+			graphFunctionName(null, userData, graphOptions);
 		}
 		else{	
 			// Invalid summonerName given
@@ -40,39 +44,102 @@ function getUserAndGraph(userName, serverName, graphFunctionName){
 		}
 	});	
 }
-function todayGraph(userData){
+
+function daysGraph(err, userData, graphOptions){
+	/*
+	Retrives games by user, between dates given
+	*/
+	// Date From -> Date To graph (each day = one data point)
+
+	var now = moment().utcOffset(offSet*-1);
+
+	// As date's are made by clientside, they are already specified to there timezone
+	var startDate = graphOptions.startDate;
+	var endDate = graphOptions.endDate;
+
+	console.log("--- Graph Type - daysGraph --- ");
+	console.log("Start Date ---> ---> " + startDate);
+	console.log("End Date ---> ---> " + endDate);
+
+	// Query Mongo DB
+	gameModel.find({$and: [{"userId":userData._id}, {dateTime: {$gt: startDate, $lt: endDate}}]}).sort({dateTime: -1}).exec(function (err, res){
+		if(err) return console.log(err);
+		// Pass found games to analysis function
+		analyzeGamesDaysGraph(null, res, userData, startDate, endDate);
+	});
+}
+function analyzeGamesDaysGraph(err, gameData, userData, startDate, endDate){
+	/*
+	Generates a data series (1 dp = 1 day)
+	Given dateFrom - dateTo &
+	User's information &
+	Game information between dates given
+	*/
+	
+	// How many data points in graph
+	var daysBetween = getHoursBetween(startDate, endDate) / 24;
+	console.log("Data points = " + daysBetween);
+
+	// Get startdate in clients timezone
+	var startDate = moment(startDate).utcOffset(offSet*-1);
+	// Log startdate in clients timezone
+	console.log(moment(startDate).format());
+
+	var tempGraphData = [];
+	for(var i=0;i<daysBetween;i++){
+		tempGraphData.push(0);
+	}
+
+	gameData.forEach(function(game){
+		// Get time in UTC
+		var a = moment(game.dateTime).utcOffset(offSet*-1);
+		
+		console.log(moment(a).format());
+		
+		// Get what datapoint this is (by getting how many days between this game and startDate)
+		var daysFromStartDate = Math.floor(getHoursBetween(startDate, moment(a).format()) / 24);
+
+		console.log("Data Point - " + daysFromStartDate);
+
+		var duration = Math.round(game.duration / 60);
+		tempGraphData[daysFromStartDate] += duration;
+	});
+
+	responder.send(tempGraphData);
+}
+
+function todayGraph(err, userData, graphOptions){
 	/*
 	Given a user, will get all the games they have played today (in there timezone)
 	*/
 	console.log("Private give me " + userData._id + " games today!");
 
 	// Step One: Get current time in UTC
-	var now = moment();
-	var currentDateClient = now.utcOffset(offSet*-1);
-
-	var todayFirstMin = currentDateClient;
+	var now = moment().utcOffset(offSet*-1);
+	
+	var todayFirstMin = now;
 	todayFirstMin = moment(todayFirstMin).seconds(00);
 	todayFirstMin = moment(todayFirstMin).minutes(00);
 	todayFirstMin = moment(todayFirstMin).hours(00);
+	todayFirstMin = moment(todayFirstMin).format();
 	// convert to javascript date for comparison in mongodb
-	todayFirstMin = moment(todayFirstMin).toDate();
 
-	var todayLastMin = currentDateClient;
+	var todayLastMin = now;
 	todayLastMin = moment(todayLastMin).seconds(59);
 	todayLastMin = moment(todayLastMin).minutes(59);
 	todayLastMin = moment(todayLastMin).hours(23);
+	todayLastMin = moment(todayLastMin).format();
 	// convert to javascript date for comparison in mongodb
-	todayLastMin = moment(todayLastMin).toDate();
 	
 	console.log("Today for client in UTC - " + todayFirstMin + " - " + todayLastMin);
 
 	gameModel.find({$and: [{"userId":userData._id}, {dateTime: {$gt: todayFirstMin, $lt: todayLastMin}}]}).sort({dateTime: -1}).exec(function (err, res){
 		if(err) return console.log(err);
-		analyzeGamesTodayGraph(userData, res, now);
+		analyzeGamesTodayGraph(userData, res);
 	});
 	
 }
-function analyzeGamesTodayGraph(userData, gameData, now){
+function analyzeGamesTodayGraph(userData, gameData){
 	/*
 	Given a list of games will slice them into each hour, so we can output to a graph
 	*/
@@ -89,17 +156,12 @@ function analyzeGamesTodayGraph(userData, gameData, now){
 	// Iterate over each game and assign to array for graphing later
 	gameData.forEach(function(game){
 		// Get time in UTC
-		var a = moment(game.dateTime);
+		var a = moment(game.dateTime).utcOffset(offSet*-1);
 		
-		// Get time in clients timezone
-		var clientDate = {
-			utc: moment(a.utc()).format(),
-			offset: offSet
-		};
-		var clientDate2 = moment.utc(clientDate.utc).zone(clientDate.offset).toDate();
-			
-		var gameDate = clientDate2;
-		var hour = gameDate.getHours();
+		console.log(moment(a).format());
+		
+		var gameDate = a;
+		var hour = moment(gameDate).hours();
 		var duration = Math.round(game.duration / 60);
 		tempGraphData[hour] += duration;
 	});
@@ -107,7 +169,10 @@ function analyzeGamesTodayGraph(userData, gameData, now){
 	responder.send(tempGraphData);
 }
 function getHoursBetween(dateOne,dateTwo){
-	var hours = Math.abs(dateOne - dateTwo) / (60*60*1000);
+	dateOne = new Date(dateOne);
+	dateTwo = new Date(dateTwo);
+
+	var hours = Math.abs(dateOne.getTime() - dateTwo.getTime()) / (60*60*1000);
 	return hours;
 }
 
