d7d7fdc6cb071b98f3d79b4757a1fd23e7794eb1
Jason <konk353535@hotmail.com>
2015-04-13 13:14:43 +1000

    Updated scanUserGames function, built a workaround for users that have no games in the riot api, but these users can still be added to the users database so this is a workaround so they won't hault other users getting updated

--- userUpdateManager.js
+++ userUpdateManager.js
@@ -31,19 +31,46 @@ var updateUser = function getUserToUpdate(){
 function scanUserGames(user){
 	/*
 	Make request for specified user's match history (using game v1.3 API)
+	Pass games found onto an analyzer
+	---- Work Around ----
+	If 404 returned, check that this is a legit user, if they are then set updated=true (some people have no games on the api server, so even thought they are a user
+	they will return a 404 for recent games as they have none
 	*/
+	console.log("Summooner Id = " + user.summonerId);
+	console.log("server = " + user.server);
 	summonerId = user.summonerId;
 	serverName = user.server;
 	// Request last 10 games
 	request('https://' + serverName + '.api.pvp.net/api/lol/' + serverName + '/v1.3/game/by-summoner/' + summonerId + '/recent?api_key=' + config.apikey , 
 	function (error, response, body) {
 		if (!error && response.statusCode == 200) {
-			console.log("Data Recievied - " + user.name);
+			console.log("Data Recievied - " + user.summonerName);
 			analyzeGames(user, JSON.parse(response.body));
 		}
 		else {
 			console.log("Invalid Summoner Name or Server");
+
+			// Run check that user is a legit user
+
+			request('https://' + serverName + '.api.pvp.net/api/lol/' + serverName + '/v1.4/summoner/by-name/' + user,summonerName + '?api_key=' + config.apikey , function (error, response, body) {
+				if (!error && response.statusCode == 200) {
+					userData = JSON.parse(response.body);
+					// Clearly a legit user
+					// Must of no previous games or match v1.3 is down
+					updateUserReset(user._id);
+				}
+				else {
+					console.log("Invalid Summoner Name or Server");
+				}
+			});
 		}
+	});
+}
+
+function updateUserReset(userId){
+	userModel.update({"_id":  userId}, { updated: true}, function(err, newInfo){
+		if(err) return handleError(err);
+		console.log("User Fully Updated");
 	});
 }
 
@@ -55,7 +82,7 @@ function analyzeGames(user, gamesData){
 	gamesData.forEach(function(game){
 		// To make sure we're only using new games (that we haven't added to DB)
 		if(game.gameId > lastMatchId){
-			console.log(game.gameId + " > " + lastMatchId);
+			// console.log(game.gameId + " > " + lastMatchId);
 			stats = game.stats;
 
 			matchId = game.gameId;
@@ -74,7 +101,7 @@ function analyzeGames(user, gamesData){
 			addGame(user, matchId, timePlayed, champion, position, dateCreated);
 		}
 		else {
-			console.log(game.gameId + " < " + lastMatchId);
+			// console.log(game.gameId + " < " + lastMatchId);
 		}
 	});
 	// Update last match id
@@ -86,10 +113,7 @@ function analyzeGames(user, gamesData){
 		});
 	}
 	// Change updated status, so we know we've scanned this user recently 
-	userModel.update({"_id":  user._id}, { updated: true}, function(err, newInfo){
-		if(err) return handleError(err);
-		console.log("User Fully Updated");
-	});
+	updateUserReset(user._id);
 }
 
 function addGame(user, newMatchId, newDuration, newChampion, newPosition, dateCreated){
@@ -98,7 +122,8 @@ function addGame(user, newMatchId, newDuration, newChampion, newPosition, dateCr
 	*/
 
 	// Format date so that it is UTC timezone
-	var dateCreatedUtc = moment.tz(dateCreated, "UTC").format());
+	var dateCreatedUtc = moment.tz(dateCreated, "UTC").format();
+	console.log("DateCreated - " + dateCreatedUtc);
 
 	var newGame = new gameModel({
 		  matchId: newMatchId
