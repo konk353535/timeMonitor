806e4cea2898adace7b72930c1b7c5175b2dd47f
Jason <konk353535@hotmail.com>
2015-04-23 09:48:36 +1000

    Updated user add manager, cast username to lowercase so we don't have to worry about mismatched cases when looking the user up.

--- userAddManager.js
+++ userAddManager.js
@@ -6,12 +6,13 @@ var request = require('request');
 var userModel = require("./models/userModel.js").userModel;
 
 // New User Functions
-var addUser = function getSummonerId(summonerName, serverName){
+var addUser = function getSummonerId(summonerName, serverName, res){
 	// Request Summoner ID
 	request('https://' + serverName + '.api.pvp.net/api/lol/' + serverName + '/v1.4/summoner/by-name/' + summonerName + '?api_key=' + config.apikey , function (error, response, body) {
 		if (!error && response.statusCode == 200) {
-			console.log("Data Recievied");
-			newUserAdd(response.body, serverName);
+			userData = JSON.parse(response.body);
+			console.log("Data Recievied - ");
+			newUserAdd(userData, serverName, res);
 		}
 		else {
 			console.log("Invalid Summoner Name or Server");
@@ -19,12 +20,11 @@ var addUser = function getSummonerId(summonerName, serverName){
 	});
 }
 
-function newUserAdd(playerJsonData, serverName){
+function newUserAdd(playerData, serverName, res){
 	/*
 	Adds new user to db, or update's existing users summoner name (incase of name change)
 	*/
 
-	playerData = JSON.parse(playerJsonData);
 	// Gets the key needed to access user data
 	for (var key in playerData) {
 	  if (playerData.hasOwnProperty(key)) {
@@ -34,10 +34,11 @@ function newUserAdd(playerJsonData, serverName){
 	// Gets user data from JSON object using key
 	playerData = playerData[playerKey];
 
-	summonerNameNew = playerData["name"];
-	summonerID = playerData["id"];
+	var summonerNameNew = playerData["name"];
+	summonerNameNew = summonerNameNew.toLowerCase();
+	var summonerID = playerData["id"];
 	console.log("Name: " + summonerNameNew + ", ID: " + summonerID + ", Server: " + serverName);
-	
+
 
 	var newUser= new userModel({
 		summonerId: summonerID,
@@ -56,6 +57,7 @@ function newUserAdd(playerJsonData, serverName){
 	userModel.update({summonerId: summonerID, server: serverName}, upsertData, {upsert: true}, function (err, newUser) {
 	  if (err) return console.error(err);
 	  console.log("User Added/Updated");
+	  res.send("PrivateEnlisted");
 	});
 }
 
