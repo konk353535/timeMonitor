2552bdc67a17f0eab63789c5299cf9c9a510ecc6
Jason <konk353535@hotmail.com>
2015-04-23 09:50:17 +1000

    Completly re constructed the graphController. Now runs on one controller, is called a set period of the page loads, the user's name and server are passed through routeParams

--- public/controllers/graphController.js
+++ public/controllers/graphController.js
@@ -1,71 +1,187 @@
-var myApp = angular.module('myCharts', ["chart.js"]);
+
+
+var myApp = angular.module('myCharts', ["chart.js", "ngRoute"]);
 
 // Access specific chart controller
-myApp.controller("todayChartCtrl", function ($scope, $http) {
-	// Request server for graph information
-	updateDailyGraph();
+myApp.controller("todayChartCtrl", ['$rootScope', '$http', '$routeParams', function ($rootScope, $http, $routeParams) {
 
-	$scope.getGraph = function(){
+	// Listen for data from user add/updated controller before we load the graph data (angular issues :())
+	// Request server for todays graph data
+	/*
+	$http.post('/userReadyForGraphing/', {userOffSet: offset, graphType: "today"}).success(function(response){
+		$scope.data = [response];
+	});
+	*/
+	$scope = $rootScope;
+	console.log($routeParams);
+	initalDailyGraph();
+	if($routeParams.userName){
 		updateDailyGraph();
 	}
 
+	function initalDailyGraph(){
+		$scope.todayChart = {};
+
+		// Today graph options
+		$scope.todayChart.labels = ['12', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '1', '2','3','4','5','6','7','8','9','10','11'];
+		$scope.todayChart.series = ['Last 24 Hours'];
+		$scope.todayChart.data= [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]];
+		$scope.options = {
+		      showTooltips: true
+		    };
+	}
 	function updateDailyGraph(){
 		// Gets clients timezone offset
 		var offset = new Date().getTimezoneOffset();
 
 		// Request server for todays graph data
-		$http.post('/graph', {userOffSet: offset, graphType: "today"}).success(function(response){
-			$scope.data = [response];
+		$http.post('/graph', {
+			userOffSet: offset,
+			graphType: "today",
+			name: $routeParams["userName"],
+			server: $routeParams["userServer"]
+		}).success(function(response){
+			$scope.todayChart.data = [response];
 		});
 	}
-	
 
-	// Today graph options
-	$scope.labels = ['12', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '1', '2','3','4','5','6','7','8','9','10','11'];
-	$scope.series = ['Last 24 Hours'];
-	$scope.data = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]];
-	$scope.options = {
-	      showTooltips: true
-	    };
 
-});
-myApp.controller("weekChartCtrl", function ($scope, $http) {
-	/*
-	Still have to update this functions commenting and spacing (need datepicker)
-	*/
 
-	var fromDate = new Date("April 7, 2015");
-	var toDate = new Date("April 12, 2015");
+	if($routeParams.userName){
+		initalMultiDayGraph();
+		updateMultiDayGraph();
+		$rootScope.userName = $routeParams.userName;
+	}
+	else {
+		initalMultiDayGraph();
+	}
 
-	console.log(fromDate);
-	console.log(toDate);
-	
-	// Request server for multi day graph information
+	$scope.getGraph = function(){
+		console.log($scope.weekChart.data);
+	}
+
+	function initalMultiDayGraph(){
+		/*
+		Still have to update this functions commenting and spacing (need datepicker)
+		Controller for multi day (7 days atm) bar chart
+		*/
+		$scope.weekChart = {};
+
+		// Mutli day Inital graph options
+		$scope.weekChart.labels = ['0','1','2','3','4'];
+		$scope.weekChart.series = ['Last 7 Days'];
+		$scope.weekChart.data = [[0, 0, 0, 0]];
+		$scope.weekChart.options = {
+		      showTooltips: true
+		};
+	}
 	function updateMultiDayGraph(){
+		var n = new Date();
+		var fromDate = new Date(n.getFullYear(), (n.getMonth()), n.getDate(), 0, 0, 0, 0);
+		var toDate = new Date(n.getFullYear(), (n.getMonth()), n.getDate(), 0, 0, 0, 0);
+
+		toDate.setDate(fromDate.getDate() + 1);
+		fromDate.setDate(fromDate.getDate() - 6);
+
 		var offset = new Date().getTimezoneOffset();
+
 		// Request server for mutli day graph data
-		$http.post('/graph', {userOffSet: offset, graphType: "daysGraph", startDate: fromDate, endDate: toDate}).success(function(response){
+
+		$http.post('/graph', {
+			userOffSet: offset,
+			graphType: "daysGraph",
+			startDate: fromDate,
+			endDate: toDate,
+			name: $routeParams["userName"],
+			server: $routeParams["userServer"]
+		}).success(function(response){
 			graphInfo = response;
 
 			console.log(graphInfo.data);
 			console.log(graphInfo.labels);
-			
-			$scope.data = [graphInfo.data];
-			$scope.labels = graphInfo.labels;
+
+			$scope.weekChart.data = [graphInfo.data];
+			$scope.weekChart.labels = graphInfo.labels;
+
+			console.log($scope.weekChart.data);
 		});
 	}
 
-	$scope.getGraph = function(){
-		updateMultiDayGraph();
+	initalMultiDayChampGraph();
+
+	if($routeParams.userName){
+
+		updateMultiDayChampGraph();
 	}
 
-	// Multi day Inital graph options
-	$scope.labels = ['0','1','2','3','4'];
-	$scope.series = ['Last 5 Days'];
-	$scope.data = [[0, 0, 0, 0]];
-	$scope.options = {
-	      showTooltips: true
-	    };
+	function updateMultiDayChampGraph(){
+		/*
+		Still have to update this functions commenting and spacing (need datepicker)
+		Controller for champion pie chart
+		*/
+		var n = new Date();
+		var fromDate = new Date(n.getFullYear(), (n.getMonth()), n.getDate(), 0, 0, 0, 0);
+		var toDate = new Date(n.getFullYear(), (n.getMonth()), n.getDate(), 23, 59, 59, 99);
+		toDate.setDate(fromDate.getDate());
+		fromDate.setDate(fromDate.getDate() - 6);
+		console.log("ToDate = " + toDate);
+		console.log("fromDate = " + fromDate);
+
+
+		var offset = new Date().getTimezoneOffset();
+
+		// Request server for mutli day graph data
+
+		$http.post('/graph', {
+			userOffSet: offset,
+			graphType: "championDaysGraph",
+			startDate: fromDate,
+			endDate: toDate,
+			name: $routeParams["userName"],
+			server: $routeParams["userServer"]
+		}).success(function(response){
+			graphInfo = response;
+
+			console.log(graphInfo.data);
+			console.log(graphInfo.labels);
+
+			$scope.champChart.data = graphInfo.data;
+			$scope.champChart.labels = graphInfo.labels;
+		});
+	}
+	function initalMultiDayChampGraph(){
+		// Inital Chart Setting for champion breakdown chart
+		$scope.champChart = {};
+		$scope.champChart.options = {tooltipTemplate: "<%if (label){%><%=label%>: <%}%><%= value %> hr"}
+	}
+
+}]);
+
+
+
+myApp.config(function($routeProvider, $locationProvider) {
+  $routeProvider
+   .when('/user/:userName/:userServer', {
+    template: ' ',
+    controller: 'todayChartCtrl',
+    resolve: {
+      // I will cause a 1 second delay
+      delay: function($q, $timeout) {
+        var delay = $q.defer();
+        $timeout(delay.resolve, 200);
+        return delay.promise;
+      }
+    }
+  })
+  .when('/Book/:bookId/ch/:chapterId', {
+    templateUrl: 'chapter.html',
+    controller: 'ChapterController'
+  });
+
+  // configure html5 to get links working on jsfiddle
+  $locationProvider.html5Mode(true);
 });
+
+
 
 angular.bootstrap(document.getElementById("chartApp"), ['myApp', 'myCharts']);
