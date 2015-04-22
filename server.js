d05896480ed92ab2248c10b611ed7782fbb5afb0
Jason <konk353535@hotmail.com>
2015-04-23 09:46:42 +1000

    Reformmated graphing function to take input from the client for the name rather than always using iyvy

--- server.js
+++ server.js
@@ -31,17 +31,30 @@ app.get('/', function (req, res) {
 	res.send('Hello World');
 })
 
+app.get('/user/:sName/:serName/', function(req, res){
+	console.log(req.params);
+	console.log("Summ Name: " + req.params.sName);
+	console.log("Server Name: " + req.params.serName);
+	res.sendFile(__dirname + '/public/user.html');
+})
+
 app.post('/graph', function(req,res){
 	var graphManager = require('./graphManager.js');
 	var graphManagerOne = graphManager;
+
+	var playerObject = req.body;
+	var playerName = playerObject["name"];
+	playerName = playerName.toLowerCase();
+
+	console.log("HERE -- " + playerName);
 	// Req.body contains graphName, clientTimeZoneOffSet, dates
-	graphManagerOne.getGraph("iyvy", "oce", req.body, res);
+	graphManagerOne.getGraph(playerName, playerObject["server"], req.body, res);
 })
 
 app.post('/newPlayer', function (req, res) {
 	console.log("Captain transmission recieved - Private Name - " + req.body.name + " - Battalion - "  + req.body.server);
-	userAddManager.addUser(req.body.name, req.body.server);
-	res.send("Roger that private, info recieved");
+	userAddManager.addUser(req.body.name, req.body.server, res);
+	//res.send("Roger that private, info recieved");
 });
 
 app.listen(3000)
