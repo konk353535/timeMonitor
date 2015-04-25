405f6c67f21daad3112f30268e9f0bd6e427f5b5
Jason <konk353535@hotmail.com>
2015-04-25 20:25:58 +1000

    Added function for retrieving a users average time played per day in minutes, lots of nested callbacks which is poor :(

--- statManager.js
+++ statManager.js
@@ -30,6 +30,9 @@ var getStats = function getStat(userName, userServer, statType, responder){
             if(statType == "recordDay"){
                 recordDay(null, userData, responder);
             }
+            else if(statType == "averageDay"){
+                averageDay(null, userData, responder);
+            }
         }
         else{
             // Invalid summonerName given
@@ -58,16 +61,20 @@ function recordDay(err, userData, responder){
         { $limit: 1}],
     function (err, res){
         if(err);
+        console.log(res);
         var recordGame = res[0];
+
         var recordMinutes = Math.round(recordGame.totalSeconds / 60);
 
         var recordDateObject = recordGame._id;
+
         var statData = {
             recordMinutes : recordMinutes,
             year: recordDateObject.year,
             month: recordDateObject.month,
             day: recordDateObject.day
         }
+
         responder.send(statData);
 
         // Flush variables ain't using em :( anymore
@@ -78,6 +85,62 @@ function recordDay(err, userData, responder){
     })
 
 }
+
+function averageDay(err, userData, responder){
+    /*
+    Calculate average time spent /day by getting totalTimePlayed / Diff(earliestGameTracked, latestGameTracked)
+    Welcome to callback HELL !!!! ><> -|- <>< ||||-- ><> ><> ><> --|||| <>< <>< <><
+    */
+    console.log("------------- AverageDay stat request ---------------");
+
+
+    // Get totalGameDuration
+    gameModel.aggregate([
+    { $match : { userId : userData._id}},
+    { $group : {
+        _id : {},
+       totalTimeSeconds : { $sum : "$duration" }}
+    }],
+    function (err, res){
+        if(err){console.log(err);}
+        console.log(res);
+        // Store totalTrackedTime
+        var totalGameStats = res[0];
+        var totalTrackedMinutes = Math.round(totalGameStats.totalTimeSeconds/60);
+        // Get Latest Game
+        gameModel.findOne({userId : userData._id}).sort({dateTime: -1}).exec(function(err,res){
+            if(err){console.log(err);}
+            var latestGame = res.dateTime;
+            // Get Earliest Game
+            gameModel.findOne({userId : userData._id}).sort({dateTime: 1}).exec(function(err,res){
+                if(err){console.log(err);}
+                console.log("Earliest Game - " + res.dateTime);
+                console.log("Latest Game - " + latestGame);
+                console.log("totalTrackedMinutes - " + totalTrackedMinutes);
+                averageDayOutputter(res.dateTime, latestGame, totalTrackedMinutes, responder);
+            })
+        })
+    });
+}
+
+function averageDayOutputter(firstGame, lastGame, totalTrackedMinutes, responder){
+    /*
+    Given users first and last game
+    Aswell as total duration tracked
+    Outputs to clients there average time played each day
+    */
+
+    // Gets total days the user has been tracked for
+    var secondsBetweenDates = Math.abs(Math.round(firstGame-lastGame)/1000);
+    var daysBetweenDates = Math.round(secondsBetweenDates / 60 / 60 / 24);
+    if(daysBetweenDates < 1){
+        daysBetweenDates = 1;
+    }
+
+    var averageMinsPerDay = Math.round(totalTrackedMinutes / daysBetweenDates);
+    responder.send([averageMinsPerDay]);
+}
+
 
 module.exports = {
     getStats: getStats
