8fd42f04c3c098635e6e16907b92339243e0c11d
Jason <konk353535@hotmail.com>
2015-04-28 13:50:51 +1000

    Added isWin to model, moved commas to right rather than left

--- models/gameModel.js
+++ models/gameModel.js
@@ -3,13 +3,14 @@ var mongoose = require("mongoose");
 // Mongoose User Schema
 // Define schema
 var GameSchema = mongoose.Schema({
-    matchId  :  { type: Number, index: true}
-  , dateTime  :  { type: Date}
-  , duration   :  { type: Number }
-  , champion :  { type: Number }
-  , position : {type: Number}
-  , server : {type: String}
-  , userId : [{type: ObjectId, ref: 'UserSchema'}]
+  matchId  :  { type: Number, index: true},
+  dateTime  :  { type: Date},
+  duration   :  { type: Number },
+  champion :  { type: Number },
+  position : {type: Number},
+  server : {type: String},
+  userId : {type: mongoose.Schema.ObjectId, ref: 'UserSchema'},
+  isWin : {type: Boolean}
   // top = pos 1, role 4
   // mid = pos 2, role 4
   // JG = pos 3
