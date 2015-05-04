e027a6f7fe574e78a0a98c6d14aadd164777258b
Jason <konk353535@hotmail.com>
2015-05-04 11:20:47 +1000

    Added a request to the server for countUsers, which will display the returned number to the screen

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,17 +1,36 @@
 var myApp = angular.module('myApp', ["chart.js"]);
 
-myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
+myApp.controller('AppCtrl', ['$scope', '$rootScope', '$http', function($scope, $rootScope, $http) {
 
-// Set default select value to oce
-$scope.player = {server: "oce", name: ""};
+    // Set default select value to oce
+    $scope.player = {server: "oce", name: ""};
 
-// Function that is called once add is hit
-$scope.addContact = function(){
-	console.log($scope.player.name);
-	console.log($scope.player.server);
-	$http.post('/newPlayer', $scope.player).success(function(response){
-		console.log(response);
-	});
-};
+    // Function that is called once add is hit
+	$scope.addContact = function(){
+        var offset = new Date().getTimezoneOffset();
+        console.log($scope.player.name);
+        console.log($scope.player.server);
+
+        var requestInfo = {
+            name : $scope.player.name,
+            server: $scope.player.server,
+            reqOffset : offset
+        };
+
+        $http.post('/newPlayer', requestInfo).success(function(response){
+            console.log(response);
+        });
+    }
+
+    // Request server for number of users
+    $http.get('/countUsers').success(function(response){
+
+        console.log(response);
+
+        // Display number of users
+        $rootScope.userCount = response[0];
+
+    });
+
 }]);
 
