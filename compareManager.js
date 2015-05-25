fd9ffee702bde2c761cba6ff96c8534dfda3b412
Jason <konk353535@hotmail.com>
2015-05-25 14:55:06 +1000

    Added scaffolding for compare manager

---
+++ compareManager.js
@@ -0,0 +1,29 @@
+// Goal is too give a front end graph data for users to
+// 	compare themselves to friends
+
+// Given a Base User
+// And Other Users
+
+// Generates Data of the set
+// [Patt, Jeffanator, Omnarino] 
+// [20.2, 40.6, 32.1]
+
+// The timezone in which all games are retrived in is based on the timezone of the base User
+
+// Data Aggregation Frame Work - Last 7 Days
+
+// 1--
+// Will get the date in the clients timezone
+// Will get the date 7 days ago in the clients-tz
+
+// 2--
+// Will change all gameDates by clientsOffset
+
+// 3-- 
+// Will select all games from [userGiven, datesGiven]
+
+// 4-- 
+// Will group games by user
+
+// 5--
+// Output what we find 
