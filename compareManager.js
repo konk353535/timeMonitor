21ef14ea77d0765135e1ae38b92120a079550cd4
Jason <konk353535@hotmail.com>
2015-05-26 11:52:53 +1000

    Successfully implemented compare manager, takes a base user and a set of other users, base users timezone is used and all users time played in the last 7 days is retrieved and outputted for easy implementation into a front end graph

--- compareManager.js
+++ compareManager.js
@@ -1,3 +1,11 @@
+// Models to interface with Mongo DataBase
+var userModel = require("./models/userModel.js").userModel;
+var gameModel = require("./models/gameModel.js").gameModel;
+
+// Moment to modify dates to specific timezones
+var moment = require('moment-timezone');
+
+
 // Goal is too give a front end graph data for users to
 // 	compare themselves to friends
 
@@ -27,3 +35,107 @@
 
 // 5--
 // Output what we find 
+
+
+var compareGraph = function findBaseUser(baseUser, otherUsers) {
+
+	// First we need the UniqueIds of the baseUser
+
+	// Query mongodb for user with name and server
+	userModel.findOne({"summonerName":baseUser.summonerName, "server":baseUser.serverName}).exec(function (err, baseUserData) {
+
+		if (err) return console.error(err);
+
+		processOtherUsers(baseUserData, otherUsers);
+
+	});
+
+}
+
+function processOtherUsers(baseUserData, otherUsers){
+	// Get info for all other users
+	userModel.find({
+		$or: otherUsers
+	}).exec(function(err,otherUsersData){
+		if(err) console.error(err);
+		getAllUsersGames(baseUserData, otherUsersData);
+	})
+
+
+
+
+}
+
+function getAllUsersGames(baseUserData, otherUsersData){
+
+	// Get list of all users info we need
+	var userList = [{userId: baseUserData._id}];
+
+	otherUsersData.forEach(function(userData){
+		userList.push({userId:userData._id});
+	});
+
+
+	// Now in the client's timezone
+	var now = moment().utcOffset(baseUserData.offset*-1);
+
+	// Get date one week ago in clients time
+	var oneWeekAgo = now;
+	oneWeekAgo = moment(oneWeekAgo).seconds(00);
+	oneWeekAgo = moment(oneWeekAgo).minutes(00);
+	oneWeekAgo = moment(oneWeekAgo).hours(00);
+	oneWeekAgo = moment(oneWeekAgo).subtract(6, 'day');
+	oneWeekAgo = moment(oneWeekAgo).format();
+	oneWeekAgo = new Date(oneWeekAgo);
+
+
+	gameModel.aggregate([
+	// Only use games from given users
+    { $match : { $or: userList }},
+    { $match : { dateTime: {$gt: oneWeekAgo}}},
+    { $group : {
+      _id : "$userId",
+      totalSeconds : { $sum: "$duration"}}
+    },
+    { $sort: {totalSeconds: -1} }], function(err,gameData){
+    	if(err) console.log(err);
+
+    	formatData(gameData, baseUserData, otherUsersData);
+    });
+   
+}
+
+/**
+	*	This function will take gameData
+	* User data and format it for output to clients graph
+	*
+**/
+
+function formatData(usersGameData, baseUserData, otherUsersData){
+
+	var graphList = [];
+
+	// Get list of all users info we need
+	var userList = [{userId: baseUserData._id, userName: baseUserData.summonerName}];
+
+	otherUsersData.forEach(function(userData){
+		userList.push({userId:userData._id, userName: userData.summonerName});
+	});
+
+	usersGameData.forEach(function(userGameData){
+		
+		userList.forEach(function(userData){
+			if(userGameData._id.toString() == userData.userId.toString()){
+				graphList.push({
+					name: userData.userName, 
+					totalSeconds: userGameData.totalSeconds})
+			}
+		});
+	});
+
+	console.log(graphList);
+}
+
+module.exports = {
+  compareGraph: compareGraph
+}
