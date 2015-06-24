27fae670247c75f856dd205efe29933b2322cca2
Jason <konk353535@hotmail.com>
2015-06-24 12:12:26 +1000

    Moved all services into there own sub directory for organization :)

---
+++ public/controllers/services/championPieGraphService.js
@@ -0,0 +1,133 @@
+myApp.factory('championPieGraphService', function(){
+  return {    
+    /**
+      * initalMultiDayChampGraph() initalizes graph settings for pie graph
+      *
+    **/
+    inital: function (){
+
+      // Object to store all info for ChampGraph
+      $scope.champChart = {};
+
+      // Champion Multi Day graph options
+      $scope.champChart.chartConfig = {
+          options: {
+              chart: {
+
+                  // Pie charts are cool!
+                  type: 'pie'
+              }
+          },
+          title: {
+
+            // Don't need no man, or title
+            text: ''
+          },
+          plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                    // Black theme
+                    style: {
+                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                    }
+                }
+            }
+          },
+
+          // Haven't got data from server yet, so still loading
+          loading: true
+      }
+
+    },
+
+    /**
+      * updateMultiDayChampGraph() request server for champion played data
+      * Once recieves graph the data given on a pie chart
+      *
+    **/
+  	update: function ($http, fD, tD){
+
+      
+      // Get current date for client
+  		//var n = new Date();
+
+  		var fromDate = new Date(
+        fD.getFullYear(), (fD.getMonth()), fD.getDate(), 0, 0, 0, 0);
+
+  		var toDate = new Date(
+        tD.getFullYear(), (tD.getMonth()), tD.getDate(), 23, 59, 59, 99);
+
+
+  		//toDate.setDate(fromDate.getDate());
+  		//fromDate.setDate(fromDate.getDate() - 6);
+      
+
+      // Store users timezone
+  		var offset = new Date().getTimezoneOffset();
+
+  		// Request server for mutli day graph data
+  		$http.post('/graph', {
+
+        // Pass users timezone to server
+  			userOffSet: offset,
+
+        // Tell server what graphType we want
+  			graphType: "championDaysGraph",
+
+        // Store dates for server to use
+  			startDate: fromDate,
+  			endDate: toDate,
+
+        // Users name and server
+  			name: $scope.userName,
+  			server: $scope.serverName
+
+  		}).success(function(response){
+
+        if(response && response != "Error: Specified user could not be found"){
+
+          // Store server response
+    			graphInfo = response;
+
+          // Output server reponse
+    			console.log("Pie Champion Data - " + graphInfo.data);
+    			console.log("Pie Champion Labels - " + graphInfo.labels);
+            
+          // Variables for custom array to send to highcharts
+          var graphData = graphInfo.data;
+          var labelData = graphInfo.labels;
+          var seriesData = [];
+
+          for(var i = 0; i < graphData.length; i++){
+            // Each data point is [label, value]
+            seriesData[i] = [labelData[i], graphData[i]];
+          }
+
+          $scope.champChart.chartConfig.series = [{
+            
+            // Size and innersize to make it a doughnut (cut out the middle)
+            size: '60%',
+            innerSize: '50%',
+            
+            // Type of chart is pie
+            type: 'pie',
+
+            // Tooltip is Hours Played: Value
+            name: 'Hours Played',
+
+            // Load in data from server response
+            data: seriesData
+
+          }];
+    			
+          // It's Loaded okay
+          $scope.champChart.chartConfig.loading = false;
+        }
+  		});
+  	}
+  }
+});
---
+++ public/controllers/services/todayGraphServices.js
@@ -0,0 +1,128 @@
+myApp.factory('todayGraphService', function(){
+  return {
+   /**
+      * initalDailyGraph() sets default setting for today graph
+      *
+      *
+    **/
+  	inital : function (){
+
+      // Create object to store all info about Today graph
+      $scope.todayChart = {};
+
+      // Today graph options
+      $scope.todayChart.chartConfig = {
+          // Type of graph
+          options: {
+              chart: {
+                  type: 'spline'
+              }
+          },
+          xAxis: {
+            type: 'datetime',
+            title : {
+              text: 'Time'
+            },
+            // Have a label every 3 hours
+            tickInterval: 3 * 3600 * 1000,
+            dateTimeLabelFormats : {
+              // Only show hour (am/pm)
+              hour:"%l %P",
+              day:"%l %P",
+            }
+          },
+          yAxis: {
+            title : {
+              text: 'Mins'
+            },
+            // Set min so we have no -10 range
+            min: 0,
+            // Set range to min amnt so 0 isn't vertically aligned
+            minRange: 40
+          },
+          series: [{
+              data: []
+          }],
+          title: {
+              text: ''
+          },
+          
+
+          // Set loading to true until we update the graph
+          loading: true
+      }
+  	},
+
+  	/**
+      * updateDailyGraph() 
+      * requests server for data to graph the 24 hours graph
+      *
+    **/
+    update: function (statService, $http){
+
+      // Gets clients timezone offset
+      var offset = new Date().getTimezoneOffset();
+
+      // Request server for todays graph data
+      $http.post('/graph', {
+
+        // Store client's timezone so graph is in there timezone
+        userOffSet: offset,
+
+        // Let server know what type of graph we want
+        graphType: "today",
+
+        // Store users name and server
+        name: $scope.userName,
+        server: $scope.serverName
+
+      }).success(function(response){
+        console.log("Day reponse + " + response);
+        // Make sure we got graph data, not an error message or null
+        if(response && response != "Error: Specified user could not be found"){
+          // Store response from server 
+          $scope.todayChart.data = response;
+
+          // Empty array for making custom labels
+          var customTodayData = [];
+          var todayChartData = $scope.todayChart.data;
+
+          for (var i = 0; i < todayChartData.length; i++){
+            // For each point load in a date with specific time
+            customTodayData[i] = [Date.UTC(2000, 1, 1, i), todayChartData[i]];
+          }
+
+          console.log("Today Date Data - " + customTodayData);
+
+          $scope.todayChart.chartConfig.series = [{
+            name: 'Mins Played',
+            data: customTodayData,
+            tooltip : {
+              dateTimeLabelFormats : {
+                // Make tooltip display 7 am (hr am/pm)
+                hour:"%l %P",
+                day:"%l %P",
+              }
+            },
+
+            // Set color of line
+            color: '#94e2e4',
+
+            // Marker is a circle not diamond plz
+            marker: {
+              symbol: "circle"
+            }
+
+          }];
+
+          // Data loaded, remove loading overlay
+          $scope.todayChart.chartConfig.loading = false;
+
+          // Update today stat now as depedant on today chart data
+          statService.getStatToday();
+        }
+      });
+    }
+
+  }
+});
