34dfc97471f170d180e6bfd2d8ab644a91270b47
Jason <konk353535@hotmail.com>
2015-06-25 17:08:23 +1000

    Added datepicker to today page, you can now select any date and see the full day for the specified date :), needs a decent amount of polish (Prettier page when they haven't played on that day, error checking, nicer code)

--- public/controllers/graphControllerToday.js
+++ public/controllers/graphControllerToday.js
@@ -1,6 +1,6 @@
 
 // Access Controller for graph page
-myApp.controller("todayChartCtrl", ['$rootScope', '$http', '$routeParams', 'utilityService' ,'statService', 'todayGraphService', 'championPieGraphService', function ($rootScope, $http, $routeParams, utilityService, statService, todayGraphService, championPieGraphService) {
+myApp.controller("todayChartCtrl", ['$scope', '$rootScope', '$http', '$routeParams', 'utilityService' ,'statService', 'todayGraphService', 'championPieGraphService', function ($normalScope, $rootScope, $http, $routeParams, utilityService, statService, todayGraphService, championPieGraphService) {
 
   // Only scope we want is the rootScope
 	$scope = $rootScope;
@@ -11,8 +11,39 @@ myApp.controller("todayChartCtrl", ['$rootScope', '$http', '$routeParams', 'util
   // Object so all our stats live in one place
 	$scope.stats = {};
 
+  // When our datepicker is opened
+  $scope.open = function($event) {
+
+    $event.preventDefault();
+    $event.stopPropagation();
+
+    $scope.opened = {};
+    $scope.opened[$event.target.id] = true;
+
+    // log this to check if its setting the log    
+    console.log($scope.opened);
+    
+  };
+
+  // When the date has changed (need to use normalScope so we can access scope of date picker)
+  $normalScope.dateChanged = function(){
+      $scope.currentDate = new Date($normalScope.demo.dtFrom);
+      var fromDate = new Date($normalScope.demo.dtFrom);
+      var toDate = new Date($normalScope.demo.dtFrom);
+
+      console.log(fromDate);
+      console.log(toDate);
+
+      // Req all graphs and stats
+      updateAllGraphsAndStats(fromDate, toDate);
+  }
+
+
+
+
   // If we have pulled users name and server from url
 	if($routeParams.userName){
+
 
     // Add/Update this user
     addUser();
@@ -33,6 +64,9 @@ myApp.controller("todayChartCtrl", ['$rootScope', '$http', '$routeParams', 'util
     if($routeParams.timePeriod == "Today"){
       var fromDate = new Date();
       var toDate = new Date();
+
+      console.log(fromDate);
+      console.log(toDate);
 
       // Req all graphs and stats
       updateAllGraphsAndStats(fromDate, toDate);
@@ -60,7 +94,7 @@ myApp.controller("todayChartCtrl", ['$rootScope', '$http', '$routeParams', 'util
     statService.getWinLossToday($rootScope.userName, $rootScope.serverName, $http);
 
     // Request 4 different graphs data
-    todayGraphService.update(statService, $http);
+    todayGraphService.update(fromDate, toDate, statService, $http);
 
   }
 
