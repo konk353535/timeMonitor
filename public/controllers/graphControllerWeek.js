bb5c1822d51f47f057a9abf8549568f206b8c2cc
Jason <konk353535@hotmail.com>
2015-07-05 22:37:35 +1000

    Using multiDay chart not todayCharting

--- public/controllers/graphControllerWeek.js
+++ public/controllers/graphControllerWeek.js
@@ -1,6 +1,6 @@
 
 // Access Controller for graph page
-myApp.controller("weekChartCtrl", ['$scope', '$rootScope', '$http', '$routeParams', 'utilityService' ,'statService', 'todayGraphService', 'championPieGraphService', function ($normalScope, $rootScope, $http, $routeParams, utilityService, statService, todayGraphService, championPieGraphService) {
+myApp.controller("weekChartCtrl", ['$scope', '$rootScope', '$http', '$routeParams', 'utilityService' ,'statService', 'multiDayGraphService', 'championPieGraphService', function ($normalScope, $rootScope, $http, $routeParams, utilityService, statService, multiDayGraphService, championPieGraphService) {
 
   // Only scope we want is the rootScope
 	$scope = $rootScope;
@@ -12,8 +12,7 @@ myApp.controller("weekChartCtrl", ['$scope', '$rootScope', '$http', '$routeParam
 
   // Object so all our stats live in one place
 	$scope.stats = {};
-
-  // When our datepicker is opened
+  
   $scope.open = function($event) {
 
     $event.preventDefault();
@@ -21,11 +20,23 @@ myApp.controller("weekChartCtrl", ['$scope', '$rootScope', '$http', '$routeParam
 
     $scope.opened = {};
     $scope.opened[$event.target.id] = true;
-
-    // log this to check if its setting the log    
-    console.log($scope.opened);
     
   };
+
+  // When our datepicker is opened
+  $normalScope.dateChanged = function(){
+      
+
+      var fromDate = new Date($normalScope.demo.dtFrom);
+      var toDate = new Date($normalScope.demo.dtFrom);
+
+      toDate.setDate(toDate.getDate() + 6);
+      
+      $scope.fromDate = fromDate;
+      $scope.toDate = toDate;
+
+      updateAllGraphsAndStats(fromDate, toDate);
+  }
 
   // If we have pulled users name and server from url
 	if($routeParams.username){
@@ -41,9 +52,12 @@ myApp.controller("weekChartCtrl", ['$scope', '$rootScope', '$http', '$routeParam
     $rootScope.serverFormatted = utilityService.serverFormat($rootScope.server);
 
     // Generate custom dates based upon timePeriod given
-    if($routeParams.timePeriod == "Today"){
-      var fromDate = new Date();
+    if($routeParams.timePeriod == "ThisWeek"){
+
       var toDate = new Date();
+      var fromDate = new Date();
+
+      fromDate.setDate(toDate.getDate() - 6);
 
       // Req all graphs and stats
       updateAllGraphsAndStats(fromDate, toDate);
@@ -52,7 +66,7 @@ myApp.controller("weekChartCtrl", ['$scope', '$rootScope', '$http', '$routeParam
   } else {
 
     // Initalize Two Graphs
-    todayGraphService.inital();
+    multiDayGraphService.inital();
     championPieGraphService.inital();
   }
 
@@ -61,15 +75,13 @@ myApp.controller("weekChartCtrl", ['$scope', '$rootScope', '$http', '$routeParam
     var username = $rootScope.username;
     var server = $rootScope.server;
 
-    championPieGraphService.update($http, fromDate, toDate);
-    statService.getStatAverageDay($http);
-    statService.getStatRecordDay($http);
+    championPieGraphService.update($http, fromDate, toDate, statService);
+
+    // Request Line Graph
+    multiDayGraphService.update($http, fromDate, toDate, statService);
 
     // Request W/L stats from server
     statService.getWinLoss($http, fromDate, toDate, username, server);
-
-    // Request Line Graph
-    todayGraphService.update($http, fromDate, toDate, statService);
 
   }
 
@@ -91,8 +103,11 @@ myApp.controller("weekChartCtrl", ['$scope', '$rootScope', '$http', '$routeParam
         console.log(response);
         if(response === "ReGraphPlz"){
           
-          var fromDate = new Date();
           var toDate = new Date();
+          var fromDate = new Date();
+
+          fromDate.setDate(toDate.getDate() - 6);
+
           updateAllGraphsAndStats(fromDate, toDate);
         }
     }).
