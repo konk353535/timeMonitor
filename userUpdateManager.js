6c759bcfe11ed0774b8ba097aa3b118c764c0e05
Jason <konk353535@hotmail.com>
2015-07-17 10:19:32 +1000

    Reworked how the system tells if a user played a new game (holds the last 10 games they played in an array instead of just a single maxId). Lots of small formatting changes to make the readability of the whole document better

--- userUpdateManager.js
+++ userUpdateManager.js
@@ -12,43 +12,38 @@ var gameModel = require("./models/gameModel.js").gameModel;
 // Converting to UTC
 var moment = require('moment-timezone');
 
-// Store if we have updated all users from last update set
+// Keeps a tally of how many users we are currently updating
 var updateNumber = 0;
-
 
 // Update User Games Functions
 var updateUser = function getUserToUpdate(){
 
+	// Only update if last update is completed
 	if(updateNumber == 0){
-		console.log("Started - " + new Date());
 		
-		// Will select a list of users to update
-		// Must be not new users (lastMatchId > 25) as new users are updated seperately
-		userModel.find({updated: false, lastMatchId:{$gt:25}}).sort({lastMatchId: 1}).limit(105).exec(function (err, userData) {
+		// Grab Users to update
+		userModel.find({updated: false, lastMatchId:{$ne:25}})
+		.sort({lastMatchId: -1})
+		.limit(150)
+		.exec(function (err, userData) {
+
 			if (err) return console.error(err);
-			// Got the user data commander!
-			// Make sure we atleast found one
-			console.log("Length - " + userData.length);
+			
 			if(userData.length > 0){
 				updateNumber += userData.length;
 				while(userData.length > 0){
-					// Lets remove him from the array, so we can use him like an object
 					var user = userData[0];
-					// Remove from array so we know when we have processed all users
 					userData.splice(0, 1);
-
 					scanUserGames(user, null);
 				}
 				userData = null;
 				user = null;
+			} else {
+				console.log("Everyone is updated");
 			}
-			else{
-			// No soilders left to update commander
-				console.log("Commander everyone is updated");
-			}
+
 		});
-	}
-	else {
+	} else {
 		console.log("Can't update yet still - " + updateNumber);
 	}
 }
@@ -63,30 +58,38 @@ function scanUserGames(user, resToClient){
 
 	var summonerId = user.summonerId;
 	var serverName = user.server;
+
+	var matchHistoryUrl = 'https://' + serverName 
+	+ '.api.pvp.net/api/lol/' + serverName 
+	+ '/v1.3/game/by-summoner/' + summonerId 
+	+ '/recent?api_key=' + config.apikey;
+
+
 	// Request last 10 games
-	request('https://' + serverName + '.api.pvp.net/api/lol/' + serverName + '/v1.3/game/by-summoner/' + summonerId + '/recent?api_key=' + config.apikey ,
+	request( matchHistoryUrl,
 	function (error, response, body) {
+
 		if (!error && response.statusCode == 200) {
-			console.log("Data Recievied - " + user.summonerName);
 			analyzeGames(user, JSON.parse(response.body), resToClient);
-		}
-		else {
-			console.log("Invalid Summoner Name or Server" + user.summonerName + " - " + user.server);
+		} else {
+			
+			var checkUserExistsUrl = 'https://' + serverName 
+			+ '.api.pvp.net/api/lol/' + serverName 
+			+ '/v1.4/summoner/by-name/' + user.summonerName 
+			+ '?api_key=' + config.apikey;
 
-			// Run check that user is a legit user
+			request(checkUserExistsUrl , function (error, response, body) {
 
-			request('https://' + serverName + '.api.pvp.net/api/lol/' + serverName + '/v1.4/summoner/by-name/' + user.summonerName + '?api_key=' + config.apikey , function (error, response, body) {
 				if (!error && response.statusCode == 200) {
+					// Legit user
 					var userData = JSON.parse(response.body);
-					// Clearly a legit user
-					// Must of no previous games or match v1.3 is down
 
+					// Must of no previous games or match v1.3 is down
 					updateNumber -= 1;
-					
 					updateUserReset(user._id);
-				}
-				else {
+				} else {
 					console.log("Invalid Summoner Name or Server");
+
 					// Flag user for not being legit
 					updateNumber -= 1;
 				}
@@ -96,23 +99,39 @@ function scanUserGames(user, resToClient){
 }
 
 function updateUserReset(userId){
-	userModel.update({"_id":  userId}, { updated: true}, function(err, newInfo){
+	userModel.update({"_id": userId}, { updated: true}, function(err, newInfo){
+
 		if(err) return handleError(err);
+
 		console.log("User Fully Updated");
 		console.log("Finished - " + new Date());
 	});
 }
 
 function analyzeGames(user, gamesData, resToClient){
-	var lastMatchId = user.lastMatchId;
+	var lMatchId = user.lastMatchId;
+	var newMatchId = [];
+	var isOldId = false;
+
+	if(lMatchId.constructor !== Array){
+		// This is because old accounts will have numerical lMatchId values
+		lMatchId = [lMatchId];
+		isOldId = true;
+	}
+
 	// So we can update the last match id of the player later
 	var currentMaxMatchId = 0;
+
 	var gamesData = gamesData.games;
+
 	gamesData.forEach(function(game){
-		// To make sure we're only using new games (that we haven't added to DB)
-		if(game.gameId > lastMatchId){
-			console.log("Game ID " + game.gameId + " lastMatchId " + lastMatchId);
-			// console.log(game.gameId + " > " + lastMatchId);
+
+		// To make sure we're only using new games
+		if(game.gameId > lMatchId[0] && lMatchId.indexOf(game.gameId) == -1){
+
+			console.log("Game ID " + game.gameId + " lMatchId " + lMatchId);
+
+			// console.log(game.gameId + " > " + lMatchId);
 			var stats = game.stats;
 
 			var matchId = game.gameId;
@@ -121,13 +140,12 @@ function analyzeGames(user, gamesData, resToClient){
 			var position = stats.playerPosition;
 			var dateCreated = game.createDate;
 			var isWin = stats.win;
+
 			if(position === undefined){
 				position = 0;
 			}
 
-			if(currentMaxMatchId < matchId){
-				currentMaxMatchId = matchId;
-			}
+			newMatchId.push(matchId);
 
 			addGame(user, matchId, timePlayed, champion, position, dateCreated,isWin);
 		}
@@ -135,26 +153,44 @@ function analyzeGames(user, gamesData, resToClient){
 			// console.log(game.gameId + " < " + lastMatchId);
 		}
 	});
-	// Update last match id
-	if(currentMaxMatchId > 0){
-		console.log("Last game played - " + currentMaxMatchId);
-		userModel.update({"_id":  user._id}, { lastMatchId: currentMaxMatchId}, function(err, newInfo){
+
+	console.log("New matches - " + newMatchId);
+
+	// Update last match ids
+	if(newMatchId.length > 0 || isOldId){
+		
+		var numOldGames = lMatchId.length;
+		var numNewGames = newMatchId.length;
+
+		// Remove smallest old id's
+		for(var i = 0; i < numOldGames+numNewGames-10; i++){
+			lMatchId.shift();
+		}
+
+		for(var i = 0; i < numNewGames; i++){
+			lMatchId.push(newMatchId.pop());
+		}
+
+		lMatchId.sort(function(a,b){
+			return a-b;
+		});		
+
+		userModel.update({"_id":  user._id}, { lastMatchId: lMatchId}, function(err, newInfo){
 			if(err) return handleError(err);
 			console.log("maxMatchId Updated");
 		});
+
 	}
-	// Change updated status, so we know we've scanned this user recently
+
 	updateUserReset(user._id);
 
-	// Is this a new user update or not
+	// Is this a new user being updated?
 	if(resToClient !== null){
 
-		// Send graphDataFound to client for regraphing
+		// Tell client to regraph
 		resToClient.send("ReGraphPlz");
-	}
-	else {
+	} else {
 		updateNumber -= 1;
-		console.log("Updating - " + updateNumber);
 	}
 	
 	gamesData = null;
@@ -183,7 +219,7 @@ function addGame(user, newMatchId, newDuration, newChampion, newPosition, dateCr
 
 	newGame.save(function (err, newGame) {
 		if (err) return console.error(err);
-		// Successfully added game to db
+
 		console.log("New Game Added");
 	});
 	user = null;
@@ -193,9 +229,13 @@ var resetAllUsers = function resetAllUsers(){
 	/*
 	Resets all users updated field to false
 	*/
-	userModel.update({}, {updated: false}, {multi: true}, function (err, numAffected) {
+	userModel.update({}, {updated: false}, {multi: true}, 
+		function (err, numAffected) {
+
 	  if (err) return console.error(err);
-	  console.log("Reset Users Updated Field");
+
+	  console.log("All users updated reset");
+
 	});
 }
 
@@ -209,17 +249,14 @@ var updateNewUser = function updateNewUser(summonerId, userServer, resToClient){
 	    // Found the user
 	    if(userData !== null){
 
-	    	// Check if new user or not
-	    	if(userData.lastMatchId == 25){
-	    		console.log(userData);
-	    		console.log("lmID" + userData.lastMatchId);
-	    		// New User
-	    		// 2) If lastMatchId is 25, send this user to update function
+	    	if(userData.lastMatchId[0] == 25){
+
+	    		// New User (Update Matches Now)
   				scanUserGames(userData, resToClient);
 	    	} else {
 
 	    		// Existing User
-	    		resToClient.send("UserUpdated");
+	    		resToClient.send("User Updated");
 	    	}
 	    }
 
