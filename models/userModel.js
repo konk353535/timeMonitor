78e6086d504b59d84c23ec7fe46a7767092b9ecc
Jason <konk353535@hotmail.com>
2015-07-27 13:28:57 +1000

    Added backLogged field, used for checking if users ranked games have already been iterated over and added to the db. Changed the comma location for readability purposes + consistancy

--- models/userModel.js
+++ models/userModel.js
@@ -3,12 +3,13 @@ var mongoose = require("mongoose");
 // Mongoose User Schema
 // Define schema
 var UserSchema = mongoose.Schema({
-    summonerId  :  { type: Number, index: true}
-  , server  :  { type: String}
-  , summonerName   :  { type: String }
-  , updated :  { type: Boolean, default: false }
-  , lastMatchId : { type:Number, default: 25}
-  , offset : {type: Number}
+    summonerId  :  { type: Number, index: true},
+    server  :  { type: String},
+    summonerName   :  { type: String },
+    updated :  { type: Boolean, default: false },
+    lastMatchId : { type: Array, default: [25]},
+    offset : {type: Number},
+    backLogged : {type: Boolean, default: false}
 });
 
 var userModel = mongoose.model('users', UserSchema);
