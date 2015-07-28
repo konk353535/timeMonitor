dbc8b834475ac1899619d0ef8f6c390fdbc94ba0
Jason <konk353535@hotmail.com>
2015-07-28 12:15:06 +1000

    Completed backLogManager, will now given a user's id in mongo db, make sure there not already backlogged, find there oldest recorded game, iterate over every ranked game in riots api and add each game to our db. It went back too mid last year for iyvy in a test run

--- backLogManager.js
+++ backLogManager.js
@@ -22,6 +22,9 @@ var config = require('./config/config');
 // Request for API Calls
 var request = require('request');
 
+// Converting to UTC
+var moment = require('moment-timezone');
+
 // Check given user isn't updated
 var backLogUser = function verifyUser(userId){
 
@@ -46,21 +49,21 @@ function getEarliestGame(user){
 
 		if(err) console.log(err);
 
-		var oldestRecordedGame = gameData.matchId;
+		var oldestRecordedGame = gameData[0].matchId;
 		var summonerId = user.summonerId;
 		var server = user.server;
 
-		var indexFrom = 2100;
-		var indexTo = 2115;
+		var indexFrom = 1500;
+		var indexTo = 1515;
 
 		var errorCount = 0;
 
-		getOldGames(oldestRecordedGame, summonerId, server, indexFrom, 
+		getOldGames(user, oldestRecordedGame, summonerId, server, indexFrom, 
 			indexTo, errorCount)
 	});
 }
 
-function getOldGames(oldestRecordedGame, summonerId, server, indexFrom, indexTo, errorCount){
+function getOldGames(user, oldestRecordedGame, summonerId, server, indexFrom, indexTo, errorCount){
 
 
 	// Iterate until we get 200 res + empty response
@@ -80,8 +83,8 @@ function getOldGames(oldestRecordedGame, summonerId, server, indexFrom, indexTo,
 
 		if (!error && response.statusCode == 200) {
 
-			var gameData = JSON.parse(response.body);
-			var numberOfKeys = Object.keys(gameData).length;
+			var gamesData = JSON.parse(response.body);
+			var numberOfKeys = Object.keys(gamesData).length;
 
 			indexFrom += 15;
 			indexTo += 15;
@@ -89,25 +92,114 @@ function getOldGames(oldestRecordedGame, summonerId, server, indexFrom, indexTo,
 			if(numberOfKeys == 0){
 				// Scan complete
 				console.log("indexFrom =" + indexFrom + "err =" + errorCount);
+
+				// Update users backLogged status
+				userBackLogged(user);
 			} else {
+
+				// Pass to analysis function
+				analyzeGameSet(user, gamesData, oldestRecordedGame);
+
 				// Keep scanning
-				getOldGames(oldestRecordedGame, summonerId, server, indexFrom, indexTo, errorCount);
+				getOldGames(user, oldestRecordedGame, summonerId, server, indexFrom, indexTo, errorCount);
 			}
 
 		} else {
-			// Inc error count by one
-
 			// Has there been too many errors to keep going?
 			
-			// Rerequest given url
-			getOldGames(oldestRecordedGame, summonerId, server, indexFrom, indexTo, errorCount+1);
+			if(errorCount >= 150){
+				// Don't Rerequest url
+			} else {
+				// Rerequest given url
+				getOldGames(user, oldestRecordedGame, summonerId, server, indexFrom, indexTo, errorCount+1);
+			}
 		}
 	});
 	
+}
+
+function analyzeGameSet(user, gamesData, oldestRecordedGame){
+
+	// Get the array of games
+	var gamesData = gamesData["matches"];
+
+	// Iterate through each game
+	gamesData.forEach(function(game){
+		
+		if(game.matchId < oldestRecordedGame){
+
+			var participants = game.participants[0];
+
+			var stats = participants["stats"];
+			var timeline = participants["timeline"];
+
+			var matchId = game.matchId;
+			var timePlayed = game.matchDuration;
+			var champion = participants.championId;
+			var dateCreated = game.matchCreation;
+
+			var position = timeline.lane;
+
+			var isWin = stats.winner;
+
+			if(position == "TOP"){
+				position = 1;
+			} else if(position == "MIDDLE"){
+				position = 2;
+			} else if(position == "JUNGLE"){
+				position = 3;
+			} else if(position == "BOTTOM"){
+				position = 4;
+			} else {
+				position = 0;
+			}
+
+			addGame(user, matchId, timePlayed, champion, position, 
+				dateCreated, isWin)
+
+
+		} else {
+			// This game has already been recorded
+
+		}
+	});
 
 }
 
+function addGame(user, newMatchId, newDuration, newChampion, newPosition, dateCreated, isWin){
+	/*
+	Given data will add game to db
+	*/
 
+	// Format date so that it is UTC timezone
+	var dateCreatedUtc = moment.tz(dateCreated, "UTC").format();
+	console.log("DateCreated - " + dateCreatedUtc);
+
+	var newGame = new gameModel({
+		matchId: newMatchId,
+		dateTime  :  dateCreatedUtc,
+		duration   :   newDuration,
+		champion :  newChampion,
+		position : newPosition,
+		server : user.server,
+		userId : user._id,
+		isWin : isWin
+	});
+
+	newGame.save(function (err, newGame) {
+		if (err) return console.error(err);
+	});
+	user = null;
+}
+
+function userBackLogged(user){
+	userModel.update({"_id": user._id}, { backLogged: true}, function(err,res){
+
+		if(err) return handleError(err);
+
+		console.log(user.summonerName + " backlogged");
+	});
+}
 
 // Functions that can be called outside this module
 module.exports = {
